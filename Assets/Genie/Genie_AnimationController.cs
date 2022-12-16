7567c1474bcc1c92b7a21b48c51de2bd1c608652
siyeon yoo <syne77777@gmail.com>
2022-12-17 01:49:38 +0900

    지하 씬바꾸고 지니위치바꿈
    제곧내

--- Assets/Genie/Genie_AnimationController.cs
+++ Assets/Genie/Genie_AnimationController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class genie_AnimationController : MonoBehaviour
+public class Genie_AnimationController : MonoBehaviour
 {
     public GameObject Genie;
    
