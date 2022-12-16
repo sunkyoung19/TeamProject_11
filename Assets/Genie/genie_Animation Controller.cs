35905feed9b5260bb776998fca6c03365b4b4020
siyeon yoo <syne77777@gmail.com>
2022-12-17 01:07:38 +0900

    지하 씬 바꾸기랑 지니 위치 바꿈
    제곧내

--- Assets/Genie/genie_Animation Controller.cs
+++ Assets/Genie/genie_Animation Controller.cs
@@ -5,30 +5,9 @@ using UnityEngine;
 public class genie_AnimationController : MonoBehaviour
 {
     public GameObject Genie;
-    Animator Anim;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Anim = Genie.GetComponent<Animator>();
-    }
-
+   
     private void OnTriggerEnter(Collider other)
     {
-        print("TriggerEnter " + other.name);
-        if (other.name == "FPSController")
-        {
-            Anim.SetInteger("DoorStatus", 1);
-        }
+        Genie.GetComponent<Animator>().SetInteger("State", 1);
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        print("TriggerExit " + other.name);
-        if (other.name == "FPSController")
-        {
-            Anim.SetInteger("DoorStatus", 2);
-        }
-    }
-
 }
